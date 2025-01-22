369d2f4504880437e3fe8e006afd990044923c11
DenisD3D <dd.denis2544@gmail.com>
2025-01-22 17:43:38 +0100

    Add evaluate and predict

--- nnUNetv2/models/15_MSSEG1_Full/constants.sh
+++ nnUNetv2/models/15_MSSEG1_Full/constants.sh
@@ -1,9 +1,10 @@
 source "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../../constants.sh"
+export nnUNet_raw nnUNet_preprocessed nnUNet_results
 
-DATASET_ID=15
+DATASET_ID=015
 DATASET_NAME=MSSEG1_Full
 DATASET_CONFIG=3d_fullres
 DATASET_PLANER=nnUNetPlannerResEncM # Default Value: "ExperimentPlanner"
 DATASET_PLAN=nnUNetResEncUNetMPlans   # Default Value: "nnUNetPlans"
 
-export DATASET_ID DATASET_NAME DATASET_CONFIG DATASET_PLANER DATASET_PLAN nnUNet_raw nnUNet_preprocessed nnUNet_results
+export DATASET_ID DATASET_NAME DATASET_CONFIG DATASET_PLANER DATASET_PLAN
