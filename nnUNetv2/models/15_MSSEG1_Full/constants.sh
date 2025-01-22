72623b4f0b886d36db32c6b0d98b96dc7b62020d
DenisD3D <dd.denis2544@gmail.com>
2025-01-22 16:30:23 +0100

    nnUNet framework fixes

--- nnUNetv2/models/15_MSSEG1_Full/constants.sh
+++ nnUNetv2/models/15_MSSEG1_Full/constants.sh
@@ -1,7 +1,9 @@
 source "$(dirname "$0")/../constants.sh"
 
-export DATASET_ID=15
-export DATASET_NAME=MSSEG1_Full
-export DATASET_CONFIG=3d_fullres
-export DATASET_PLANER=nnUNetPlannerResEncL # Default Value: "ExperimentPlanner"
-export DATASET_PLAN=nnUNetResEncUNetMPlans   # Default Value: "nnUNetPlans"
+DATASET_ID=15
+DATASET_NAME=MSSEG1_Full
+DATASET_CONFIG=3d_fullres
+DATASET_PLANER=nnUNetPlannerResEncL # Default Value: "ExperimentPlanner"
+DATASET_PLAN=nnUNetResEncUNetMPlans   # Default Value: "nnUNetPlans"
+
+export DATASET_ID DATASET_NAME DATASET_CONFIG DATASET_PLANER DATASET_PLAN nnUNet_raw nnUNet_preprocessed nnUNet_results
