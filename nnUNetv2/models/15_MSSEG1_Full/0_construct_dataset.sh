d35d2b7a6ebc5a6ed45b1350b2fde3924e2134f2
DenisD3D <dd.denis2544@gmail.com>
2025-01-22 16:33:02 +0100

    nnUNet framework fixes

--- nnUNetv2/models/15_MSSEG1_Full/0_construct_dataset.sh
+++ nnUNetv2/models/15_MSSEG1_Full/0_construct_dataset.sh
@@ -1,7 +1,7 @@
 source "$(dirname "$0")/constants.sh"
 
 python "$(dirname "$0")/../../utils/construct_dataset.py" \
-    --input_dir "$HOME/workspace/MSSEG-1-preprocessed-2"\
-    --input_dir "$HOME/workspace/MSSEG-1-test-preprocessed-2" \
+    --input "$HOME/workspace/MSSEG-1-preprocessed-2"\
+    --input "$HOME/workspace/MSSEG-1-test-preprocessed-2" \
     --dataset-id "$DATASET_ID" \
     --dataset-name "$DATASET_NAME"
