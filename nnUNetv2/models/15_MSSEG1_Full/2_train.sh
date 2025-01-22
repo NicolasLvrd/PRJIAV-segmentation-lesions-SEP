52a5064d830c163184291fcc23abbc1876899cde
DenisD3D <dd.denis2544@gmail.com>
2025-01-22 17:00:14 +0100

    nnUNet framework fixes

--- nnUNetv2/models/15_MSSEG1_Full/2_train.sh
+++ nnUNetv2/models/15_MSSEG1_Full/2_train.sh
@@ -1,3 +1,9 @@
 source "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/constants.sh"
 
-nnUNetv2_train "$DATASET_ID" "$DATASET_CONFIG" 0 -p "$DATASET_PLAN"
+if [ -z "$1" ]; then
+  echo "Please provide the fold number as an argument (between 0 and 4)."
+  exit 1
+fi
+FOLD=$1
+
+nnUNetv2_train "$DATASET_ID" "$DATASET_CONFIG" "$FOLD" -p "$DATASET_PLAN"
