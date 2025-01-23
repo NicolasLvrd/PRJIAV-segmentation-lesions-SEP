458afdf8783a6d652ec4e471c0f0f9e37189891a
DenisD3D <dd.denis2544@gmail.com>
2025-01-23 15:07:09 +0100

    Fix 4_evaluate.sh

--- nnUNetv2/models/15_MSSEG1_Full/4_evaluate.sh
+++ nnUNetv2/models/15_MSSEG1_Full/4_evaluate.sh
@@ -1,6 +1,6 @@
 source "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/constants.sh"
 
-nnUNetv2_evaluate_folder "${nnUNet_raw}/Dataset${DATASET_ID}_${DATASET_NAME}/labelTs/" \
+nnUNetv2_evaluate_folder "${nnUNet_raw}/Dataset${DATASET_ID}_${DATASET_NAME}/labelsTs/" \
     "${nnUNet_raw}/Dataset${DATASET_ID}_${DATASET_NAME}/predictTs" \
     -djfile "${nnUNet_raw}/Dataset${DATASET_ID}_${DATASET_NAME}/dataset.json" \
-    -pfile "${nnUNet_results}/Dataset${DATASET_ID}_${DATASET_NAME}/plans.json"
+    -pfile "${nnUNet_results}/Dataset${DATASET_ID}_${DATASET_NAME}/nnUNetTrainer__${DATASET_PLAN}__${DATASET_CONFIG}/plans.json"
